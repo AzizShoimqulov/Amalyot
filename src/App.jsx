00d9f78e46ed680c7a2a0d16d186babf727f84a2
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-05-28 17:15:59 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchForm from './SearchForm'
+import ThemeToggle from './ThemeToggle'
+import Gallery from './Gallery'
+const App = () => {
+  return (
+    <div>
+      <SearchForm />
+      <ThemeToggle />
+      <Gallery />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Gallery.jsx
@@ -0,0 +1,69 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useGlobalContext } from './context';
+import { BsHeart, BsHeartFill } from 'react-icons/bs';
+
+const url = 'https://api.unsplash.com/search/photos?client_id=xH2dss2cIxvpVJ1yiprAgYw0DWIcu3JBtgqVuVT4iaM&per_page=28';
+
+const Gallery = () => {
+  const { searchTerm } = useGlobalContext();
+  const [likedImages, setLikedImages] = useState(new Set());
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ['images', searchTerm],
+    queryFn: async () => {
+      const response = await axios.get(`${url}&query=${searchTerm}`);
+      return response.data;
+    },
+    keepPreviousData: true,
+  });
+
+  const toggleLike = (id) => {
+    setLikedImages(prev => {
+      const newSet = new Set(prev);
+      if (newSet.has(id)) newSet.delete(id);
+      else newSet.add(id);
+      return newSet;
+    });
+  };
+
+  if (isLoading) {
+    return (
+      <section className="image-container loading">
+        <div className="spinner"></div>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className="image-container">
+        <h1>Error...</h1>
+      </section>
+    );
+  }
+
+
+
+  return (
+    <section className="image-container">
+      {data.results.map(item => {
+        const imgUrl = item.urls.regular;
+        const liked = likedImages.has(item.id);
+        return (
+          <div key={item.id} className="img-wrapper">
+            <img
+              src={imgUrl}
+              className="img"
+              alt={item.alt_description || 'unsplash image'}
+            />
+
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Gallery;
